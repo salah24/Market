7b8979d36ac5612cdf6365a6e315110ce6ddc639
salah24 <salahcyb@gmail.com>
2023-02-26 11:08:44 +0100

    finish animation splash view

--- lib/features/splash/presentation/widgets/splash_body.dart
+++ lib/features/splash/presentation/widgets/splash_body.dart
@@ -7,28 +7,48 @@ class SplashBody extends StatefulWidget {
   State<SplashBody> createState() => _SplashBodyState();
 }
 
-class _SplashBodyState extends State<SplashBody> {
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
   AnimationController? animationController;
 
   Animation? fadingAnimation;
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+
+    animationController?.repeat(reverse: true);
+  }
+
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Spacer(),
-          Text(
-            'Fruit Market',
-            style: TextStyle(
-                fontSize: 50,
-                fontFamily: 'Poppins',
-                color: Color(0xffffffff),
-                fontWeight: FontWeight.bold),
+          AnimatedBuilder(
+            animation: fadingAnimation!,
+            builder: (context, _) => Opacity(
+              opacity: fadingAnimation?.value,
+              child: Text(
+                'Fruit Market',
+                style: TextStyle(
+                    fontSize: 50,
+                    fontFamily: 'Poppins',
+                    color: Color(0xffffffff),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
           Spacer(),
           Image.asset('assets/images/splash1.png'),
-          Spacer(),
+          SizedBox(
+            height: 30,
+          )
         ],
       ),
     );
